1257b1e11065226b3702ed4197573d90bb474278
Avinash <palakurthiavinash07@gmail.com>
2023-10-17 12:24:57 +0530

    title size and other things changed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import { CiLogin, CiSearch } from "react-icons/ci";
 import { RxHamburgerMenu } from "react-icons/rx";
 const Navbar = () => {
   return (
-    <div className="bg-black  h-14  sm:h-auto md:h-auto lg:h-auto text-white flex justify-evenly items-center">
-      <div className="  flex justify-between  sm:pl  lg:px-2 lg:pl-[3rem]">
+    <div className="bg-black  h-auto  sm:h-auto md:h-auto lg:h-auto text-white flex justify-evenly items-center  lg:shadow-md  lg:shadow-red-700 shadow-red-700 shadow-sm">
+      <div className="  flex justify-between  sm:pl  ">
         <h2 className="text-red-700 font-semibold text-[30px] mt-1 sm:text-[1.5rem] md:text-[2rem] lg:pl-8 pr-3">
           RedPill
         </h2>
@@ -14,7 +14,7 @@ const Navbar = () => {
           </button>
         </div>
       </div>
-      <div className="lg:flex sm:flex md:flex pl-[18px] text-[8px] gap-2 sm:text-[14px] md:text-[14px] md:pl-6 flex-wrap lg:pl-[5rem] lg:gap-5 items-center hidden ">
+      <div className="lg:flex sm:flex md:flex pl-[18px] text-[8px] gap-2 sm:text-[14px] md:text-[14px] md:pl-6 flex-wrap lg:pl-[1rem]  lg:font-thin items-center hidden ">
         <button className="capitalize hover:text-red-600 lg:text-[1.3rem] lg:px-4 ease-in-out duration-300">
           Home
         </button>
@@ -28,16 +28,44 @@ const Navbar = () => {
           technology
         </button>
         <button className="capitalize hover:text-red-600  ease-in-out duration-300 lg:text-[1.3rem] lg:px-4">
-          travel{" "}
+          travel
         </button>
-        <p className="border-r h-6 border-gray-500 md:pl-6 lg:pl-[2rem] "></p>
       </div>
-      <div className="lg:hidden  md:hidden justify-center  items-center flex">
-        <RxHamburgerMenu className=" text-white hover:text-emerald-400" />
+      <div className="dropdown sm:hidden md:hidden lg:hidden xl:hidden ">
+        <label
+          tabIndex={0}
+          className=" bg-black border-emerald-500 text-emerald-400 "
+        >
+          <RxHamburgerMenu className="  hover:text-emerald-400" />
+        </label>
+        <div
+          tabIndex={0}
+          className="dropdown-content z-[1] menu-vertical   shadow   text-white rounded-lg w-[8rem] bg-black  flex   items-center justify-start"
+        >
+          <button className="capitalize hover:text-red-600 lg:text-[1.3rem] lg:px-4 ease-in-out duration-300">
+            Home
+          </button>
+          <button className="capitalize ease-in-out duration-300 lg:text-[1.3rem] hover:text-red-600 lg:px-4">
+            news
+          </button>
+          <button className="capitalize hover:text-red-600 lg:text-[1.3rem] ease-in-out duration-300 lg:px-4">
+            culture
+          </button>
+          <button className=" ease-in-out duration-300 capitalize hover:text-red-600 lg:text-[1.3rem] lg:px-4">
+            technology
+          </button>
+          <button className="capitalize hover:text-red-600  ease-in-out duration-300 lg:text-[1.3rem] lg:px-4">
+            travel
+          </button>
+        </div>
       </div>
-      <div className="flex items-center justify-center  lg:pl-[3rem] pr-2 outline-none border-none ">
+
+      <div className="flex items-center justify-center  lg:pl-[1rem] pr-2 outline-none border-none ">
         <CiSearch className="text-white text-sm lg:text-[1.5rem]  " />
-        <input placeholder="search " className="bg-slate-200 w-[4rem] " />
+        <input
+          placeholder="search "
+          className="bg-slate-200 w-[4rem] lg:w-[6rem]"
+        />
       </div>
     </div>
   );
